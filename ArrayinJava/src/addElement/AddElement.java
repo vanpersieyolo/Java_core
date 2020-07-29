852e3d4a6aa3c56255ff6eeed9d3b2f63b00ae7e
DANG ANH VAN <you@example.com>
2020-07-30 00:23:20 +0700

    van

--- ArrayinJava/src/addElement/AddElement.java
+++ ArrayinJava/src/addElement/AddElement.java
@@ -12,19 +12,21 @@ public class AddElement {
     public static void EnterArray1 (int [] a ){
         System.out.println("nhập vào 1 chuỗi 10 phần tử: ");
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i <5 ; i++) {
+        for (int i = 0; i <6 ; i++) {
             a[i]= scanner.nextInt();
         }
     }
     public static void addElement(int[]b) {
 
-        System.out.println("nhâp vào giá trị cần thêm: ");
-        System.out.println("thêm phần tử tại vị trí sô:( 0 -> 9) ");
-        Scanner scanner = new Scanner(System.in);
-        int enterTrim = scanner.nextInt();
-        int temp = scanner.nextInt();
+        System.out.println("thêm  phần tử tại vị trí sô:( 0 -> 9) ");
+        Scanner scanne = new Scanner(System.in);
+        int enterTrim = scanne.nextInt();
+        System.out.println("thêm phần tử số  ");
+        int valueadd = scanne.nextInt();
+
         for (int i = enterTrim; i <b.length-1 ; i++) {
-            b[i] = temp;
+            b[i + 1] = b[i];
+            b[i] = valueadd;
 
         }
 
