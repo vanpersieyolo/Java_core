e34ac58aa05c3b3a74ec030bd1f1862b6540b7a1
DANG ANH VAN <you@example.com>
2020-08-05 23:59:46 +0700

    van

--- ExcerciseTDD/src/NextDayCaculator.java
+++ ExcerciseTDD/src/NextDayCaculator.java
@@ -1,14 +1,16 @@
 public class NextDayCaculator{
-    int enterDay = 0;
-    int enterMonth = 0;
-    int enterYear = 0;
+    int enterDay = 1;
+    int enterMonth = 1;
+    int enterYear = 1;
 
-    public NextDayCaculator(int enterYear, int enterDay, int enterMonth){
+    public NextDayCaculator(int enterDay, int enterMonth,int enterYear){
       this.enterDay = enterDay;
       this.enterMonth = enterMonth;
       this.enterYear = enterYear;
     }
-//    public static void getEnterMonth(int enterMonth){
+
+
+    //    public static void getEnterMonth(int enterMonth){
 //        boolean monthinput = enterMonth < 12;
 //        boolean monthinput1 = enterMonth > 1;
 //        if (monthinput && monthinput1){
@@ -22,54 +24,97 @@ public class NextDayCaculator{
                 case 1,3,5,7,8,10,12 :
                     if (enterDay < 31 && enterDay > 0){
                     enterDay += 1;
-                }else if (enterDay == 31){
-                    enterDay = 1;
-                    if (enterMonth == 12){
-                        enterYear+=1;
-                        enterMonth = 1;
-                    }else {
-                        enterMonth =1;
+                    System.out.println("ngày tiếp theo là: " +enterDay + " / "+ enterMonth+ " / "+ enterYear);
+                    break;
                     }
-                    }else System.out.println(" ngày nhập vào không có");
+                    if (enterDay == 31){
+                        if (enterMonth == 12){
+                            enterYear+=1;
+                            enterMonth = 1;
+                            enterDay = 1;
+                        }else {
+                            enterMonth +=1;
+                            enterDay = 1;
+                        }
+                        System.out.println("ngày tiếp theo là: " +enterDay + " / "+ enterMonth+ " / "+ enterYear);
+                        break;
+                    }
+                    if (enterDay > 31 || enterDay < 0){
+                        System.out.println("không có ngày xác định 1");
+                    }
                 break;
                 case 4,6,9,11 :
                     if (enterDay < 30 && enterDay > 0){
                         enterDay += 1;
-                    }else if (enterDay == 30){
-                        enterDay = 1;
-                        enterMonth += 1;
-                    }else System.out.println(" ngày nhập vào không có");
-                    break;
-                case 2:
-                    if (enterYear % 4 == 0){
-                        if (enterYear % 100 != 0){
-                            if (enterDay < 30 && enterDay > 0){
-                                enterDay += 1;
-                            }else if (enterDay == 29){
-                                enterDay = 1;
-                                enterMonth+= 1;
-                            }else System.out.println(" ngày nhập vào không có");
-                            break;
-                        }else {
-                            if (enterDay < 29 && enterDay > 0){
-                                enterDay += 1;
-                            }else if (enterDay == 28){
-                                enterDay = 1;
-                                enterMonth+= 1;
-                            }else System.out.println(" ngày nhập vào không có");
-                            break;
-                        }
-                    }else {
-                        if (enterDay < 29 && enterDay > 0){
-                            enterDay += 1;
-                        }else if (enterDay == 28){
-                            enterDay = 1;
-                            enterMonth+= 1;
-                        }else System.out.println(" ngày nhập vào không có");
+                        System.out.println("ngày tiếp theo là: " +enterDay + " / "+ enterMonth+ " / "+ enterYear);
                         break;
                     }
-                }
+                    if (enterDay == 30){
+                            enterMonth +=1;
+                            enterDay =1;
+                        System.out.println("ngày tiếp theo là: " +enterDay + " / "+ enterMonth+ " / "+ enterYear);
+                        break;
+                        }
+                    if (enterDay > 30 || enterDay < 0){
+                        System.out.println("không có ngày xác định");
+                    }
+                    break;
+                case 2:
+                    if (enterYear % 4 == 0) {
+                        if (enterYear % 100 != 0) {
+                            if (enterDay < 29 && enterDay > 0) {
+                                enterDay += 1;
+                                System.out.println("ngày tiếp theo là: " +enterDay + " / "+ enterMonth+ " / "+ enterYear);
+                                break;
+                            }
+                            if (enterDay == 29) {
+                                enterMonth += 1;
+                                enterDay = 1;
+                                System.out.println(" đây là năm nhuận nên sẽ có 29 ngày của tháng 2 !");
+                                System.out.println("ngày tiếp theo là: " +enterDay + " / "+ enterMonth+ " / "+ enterYear);
+                                break;
+                            }
+                            if (enterDay > 29 || enterDay < 0) {
+                                System.out.println("không có ngày xác định");
+                            }
+                            break;
+                        }else if(enterYear % 400 == 0){
+                            if (enterDay < 29 && enterDay > 0) {
+                                enterDay += 1;
+                                System.out.println("ngày tiếp theo là: " +enterDay + " / "+ enterMonth+ " / "+ enterYear);
+                                break;
+                            }
+                            if (enterDay == 29) {
+                                enterMonth += 1;
+                                enterDay = 1;
+                                System.out.println(" đây là năm nhuận nên sẽ có 29 ngày của tháng 2 !");
+                                System.out.println("ngày tiếp theo là: " +enterDay + " / "+ enterMonth+ " / "+ enterYear);
+                                break;
+                            }
+                            if (enterDay > 29 || enterDay < 0) {
+                                System.out.println("không có ngày xác định");
+                            }
+                            break;
+                        }else {
+
+                            if (enterDay < 28 && enterDay > 0) {
+                                enterDay += 1;
+                                System.out.println("ngày tiếp theo là: " +enterDay + " / "+ enterMonth+ " / "+ enterYear);
+                                break;
+                            }
+                            if (enterDay == 28) {
+                                enterMonth += 1;
+                                enterDay = 1;
+                                System.out.println("ngày tiếp theo là: " +enterDay + " / "+ enterMonth+ " / "+ enterYear);
+                                break;
+                            }
+                            if (enterDay > 28 || enterDay < 0) {
+                                System.out.println("không có ngày xác định");
+                            }
+                        }
+                    }
         }
 
+    }
 }
 
--- ExcerciseTDD/src/Show.java
+++ ExcerciseTDD/src/Show.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Show {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhập vào ngày: ");
+        int enterDay = scanner.nextInt();
+        System.out.println("nhập vào tháng: ");
+        int enterMonth = scanner.nextInt();
+        System.out.println("nhập vào năm: ");
+        int enterYear = scanner.nextInt();
+
+       NextDayCaculator nextDayCaculator = new NextDayCaculator(enterDay,enterMonth,enterYear);
+       nextDayCaculator.nextDay(enterDay,enterMonth,enterYear);
+    }
 }
