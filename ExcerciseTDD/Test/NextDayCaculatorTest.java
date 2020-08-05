cb489e1a4e33b9c25780c67de73f5f292e88c1f0
= <=>
2020-08-05 21:11:06 +0700

    van

---
+++ ExcerciseTDD/Test/NextDayCaculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NextDayCaculatorTest {
+
+    @Test
+    void checkDay(int day) {
+        day += 1;
+    }
+}
